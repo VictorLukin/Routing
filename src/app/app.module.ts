4cd6295dd08287143ae67fa321eae020dd980b03
V1S1TER-PC\V1s1ter <lukin@fastmail.com>
2019-10-20 11:16:00 +0300

    Initialise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Parent1Component } from './Pages/parent1/parent1.component';
+import { Parent2Component } from './Pages/parent2/parent2.component';
+import { Parent3Component } from './Pages/parent3/parent3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Parent1Component,
+    Parent2Component,
+    Parent3Component
   ],
   imports: [
     BrowserModule
